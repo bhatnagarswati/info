8d3297eb5bb979407ed0e54637b7d0a9be700d25
ritu kashyap <ritukashyap478@gmail.com>
2019-06-16 15:46:01 +0530

    added contact form

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $db_host = 'localhost';
     $db_username = 'root';
-    $db_password = '';
+    $db_password = 'demanding0605';
     $db_name = 'bhatnagars';
 
     $conn = mysqli_connect( $db_host, $db_username, $db_password, $db_name) or die("Connection Error: " . mysqli_error($conn));
